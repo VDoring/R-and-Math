c28716f1111a8e16ef004478a0302045bcdb718d
Doring <d0211468@kowon.dongseo.ac.kr>
2021-09-26 15:41:39 +0900

    행렬의 전치행렬

---
+++ 1-2 선형대수/vid2_2.R
@@ -0,0 +1,11 @@
+A = matrix(c(2,3,3,4,-1,2),ncol=3)
+A # 행렬 A
+t(A) # 행렬 A의 전치행렬
+
+B = matrix(c(1,-1,2,3,2,0),ncol=3)
+B # 행렬 B
+t(B)  # 행렬 B의 전치행렬
+
+C = matrix(c(5,2,6),ncol=1)
+C # 행렬 B
+t(C)  # 행렬 B의 전치행렬
